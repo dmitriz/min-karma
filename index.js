83d9bdec209d3ba4c653304a8bd62e476dffa24e
dmitriz <dmitri14@gmail.com>
2016-01-14 07:35:03 +0000

    Fix copy files

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 
 var path = require('path');
 var fs = require('fs');
-var cpy = require('cpy');
+//var cpy = require('cpy');
 
 // Current directory inside the package
 var packagePath = path.resolve();
 
 var filesToCopy = ['karma*', 'src'];
 
+var sourceFile = 'one';
+var targetFile = 'two';
+
 
 // Moving files to the local directory
 
-cpy(filesToCopy, '.']).then(function(result) {
-	console.log(result, 'files copied');
-});
+// cpy(filesToCopy, '.']).then(function(result) {
+// 	console.log(result, 'files copied');
+// });
 
-//fs.writeFileSync(targetFile, fs.readFileSync(sourceFile));
+fs.writeFileSync(targetFile, fs.readFileSync(sourceFile));
+
 //fs.createReadStream(packagePath + '/karma.conf.js').pipe(fs.createWriteStream('karma.conf.js'));
