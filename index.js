5f37560d8564764d796006a1d0aabae7b50827f5
dmitriz <dmitri14@gmail.com>
2016-01-14 07:12:01 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // index.js
 
-var packagePath = require('min-karma');
-
+var path = require('path');
 var fs = require('fs');
+var cpy = require('cpy');
+
+// Current directory inside the package
+var packagePath = path.resolve();
+
+var filesToCopy = ['karma*', 'src'];
+
 
 // Moving files to the local directory
+
+cpy(filesToCopy, '.']).then(function(result) {
+	console.log(result, 'files copied');
+});
+
 //fs.writeFileSync(targetFile, fs.readFileSync(sourceFile));
-fs.createReadStream(packagePath + 'karma.conf.js').pipe(fs.createWriteStream('karma.conf.js'));
+//fs.createReadStream(packagePath + '/karma.conf.js').pipe(fs.createWriteStream('karma.conf.js'));
