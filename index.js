9ad44029a3147040a0d70aa6699f32af281739b5
dmitriz <dmitri14@gmail.com>
2016-01-14 11:03:03 +0000

    Try __dirname

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ require('shelljs/global');
 // Current directory inside the package
 var packagePath = path.resolve();
 
+// Current directory of the user
+var userPath = __dirname;
+
+
+console.log("packagePath is", packagePath);
+
 // Moving files to the local directory
 var filesToCopy = ['karma.conf.js', 'src'];
-console.log("trying to copy files...", filesToCopy);
+console.log("trying to copy files...", filesToCopy, userPath);
 
 filesToCopy.forEach(function(file){
 	// testing
 	// cp('-R',  file, packagePath + '/dist');
 	console.log('Copying', file); 
-	cp('-R', file, '.');
+	cp('-R', file, userPath);
 });
