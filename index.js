7d210ca035627cd7e7f0c29e2266a89c21a2d775
dmitriz <dmitri14@gmail.com>
2016-01-14 14:57:11 +0000

    Cleanup index.js

--- index.js
+++ index.js
@@ -7,23 +7,14 @@ var fs = require('fs');
 // https://github.com/shelljs/shelljs#javascript
 require('shelljs/global');
 
-// Current directory inside the package
-var packagePath = path.resolve();
-var packagePath = __dirname;
-
-// Current directory of the user
+// TODO: Find a more robust solution
+// Current user directory
 var userPath = "../..";
 
-
-console.log("packagePath is", packagePath);
-
 // Moving files to the local directory
-var filesToCopy = ['./karma.conf.js', 'src'];
-console.log("trying to copy files...", filesToCopy, userPath);
+var filesToCopy = ['karma.conf.js', 'src'];
 
 filesToCopy.forEach(function(file){
-	// testing
-	// cp('-R',  file, packagePath + '/dist');
 	console.log('Copying', file); 
 	cp('-R', file, userPath);
 });
