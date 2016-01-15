97e97eb6a70d2103604e1aed84dfdaba3f14d33c
dmitriz <dmitri14@gmail.com>
2016-01-15 03:00:03 +0000

    Update examples

--- examples/add_test.js
+++ examples/add_test.js
@@ -1,7 +1,7 @@
 // add_test.js
 
 describe('Addition', function(){
-	it('should add', function(){
+	it('should add numbers', function(){
 		expect(add(2,4)).toBe(6);
 		expect(add(2,4)).not.toBe(2);
 	});
