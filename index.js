7c2859463c0d96079cc506371d00989632d70a9e
dmitriz <dmitri14@gmail.com>
2016-01-15 12:58:12 +0000

    Change to spawn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@
 // import shell tools for global access
 // https://github.com/shelljs/shelljs#javascript
 require('shelljs/global')
+var spawn = require('cross-spawn-async')
 
 // TODO: Find a more robust solution
 // Current user directory
@@ -22,5 +23,7 @@ filesToCopy.forEach(function (file) {
   console.log('Copying file or directory: ', file)
 
   // cp is defined through shelljs
-  cp('-R', file, userPath)
+  //cp('-R', file, userPath)
+	spawn('cp', [file, __dirname], {stdio: 'inherit'})
+
 })
