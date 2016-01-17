42c291b15ac7aa958ab6c9c37e647fbcc411ea96
dmitriz <dmitri14@gmail.com>
2016-01-17 12:30:47 +0000

    Update index.js: remove global shelljs

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // index.js
-/* globals cp */
 
 /*
  * Script to run after npm install
@@ -7,20 +6,18 @@
  * Currently: copying selected files to user's directory
  */
 
-// import shell tools for global access
 // https://github.com/shelljs/shelljs#javascript
-require('shelljs/global')
+var shells = require('shelljs')
 
-// TODO: Find a more robust solution
 // Current user directory
+// Warning: This assumes the package is installed into `node_modules/<package-name>/`
+// TODO: Find a more robust solution
 var userPath = '../../'
 
-// Moving files to the local directory
+// Moving files to user's local directory
 var filesToCopy = ['karma.conf.js', 'examples']
 
 filesToCopy.forEach(function (file) {
   console.log('Copying file or directory: ', file)
-
-  // cp is defined through shelljs
-  cp('-R', file, userPath)
+  shells.cp('-R', file, userPath)
 })
