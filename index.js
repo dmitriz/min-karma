3e5df94e614fb5fa7c8c6406bac3b7e22ec5d37c
dmitriz <dmitri14@gmail.com>
2016-05-09 09:55:55 +0100

    Move old karma.conf.js to a copy

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 'use strict'
 
+var fs = require('fs')
 var gentlyCopy = require('gently-copy')
 
 var filesToCopy = ['karma.conf.js', 'demo']
@@ -15,6 +16,21 @@ var filesToCopy = ['karma.conf.js', 'demo']
 // User's local directory
 // Warning: This assumes the package is installed into `node_modules/<package-name>/`
 var userPath = '../../'
+
+
+// If `karma.conf.js` already exists, move it to karma.conf_copy.js
+var newConf = 'karma.conf.js'
+var oldConf = userPath + newConf
+var oldConfCopy = oldConf + userPath + 'karma.conf_copy.js'
+var movedConf = false;
+
+try {
+	// fs.accessSync(oldConf)
+	fs.renameSync(oldConf, oldConfCopy)
+	movedConf = true;
+} catch (e) {
+    // No old config
+}
 
 // Moving files to user's local directory
 gentlyCopy(filesToCopy, userPath)
