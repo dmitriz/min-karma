1763054e7bab35d3c626dcd10a621a1cccae6757
David J. Bradshaw <david.j.bradshaw@gmail.com>
2019-03-06 17:27:27 +0000

    Fix user path
    This script just helped me out, so I though I would share the fix I made to it with you

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ var gentlyCopy = require('gently-copy')
 var filesToCopy = ['karma.conf.js', 'demo']
 
 // User's local directory
-// Warning: This assumes the package is installed into `node_modules/<package-name>/`
-var userPath = '../../'
+var userPath = process.env.INIT_CWD
 
 // Moving files to user's local directory
 gentlyCopy(filesToCopy, userPath)
